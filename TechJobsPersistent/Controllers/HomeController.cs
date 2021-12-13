cb705642d5bb594403ec8d4237601b1e964a99c5
Kenneth Jackson <kcjackson1985@gmail.com>
2021-12-13 16:01:25 -0500

    My head hurts

--- TechJobsPersistent/Controllers/HomeController.cs
+++ TechJobsPersistent/Controllers/HomeController.cs
@@ -32,13 +32,44 @@ namespace TechJobsPersistent.Controllers
         [HttpGet("/Add")]
         public IActionResult AddJob()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+            List<Skill> skills = context.Skills.ToList();
+            AddJobViewModel addJobViewModel = new AddJobViewModel(employers, skills);
+            return View(addJobViewModel);
         }
 
-        public IActionResult ProcessAddJobForm()
+        public IActionResult ProcessAddJobForm(AddJobViewModel addJobViewModel, string[] selectedSkills)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = addJobViewModel.Name,
+                    EmployerId = addJobViewModel.EmployerId,
+                    Employer = context.Employers.Find(addJobViewModel.EmployerId),
+                };
+
+                foreach (string item in selectedSkills)
+                {
+                    JobSkill newJobSkill = new JobSkill
+                    {
+                        JobId = newJob.Id,
+                        Job = newJob,
+                        SkillId = Int32.Parse(item),
+                        Skill = context.Skills.Find(Int32.Parse(item))
+                    };
+                    context.JobSkills.Add(newJobSkill);
+                }
+
+                context.Jobs.Add(newJob);
+                context.SaveChanges();
+
+                return Redirect("Index");
+            }
+
+            return View("Add", addJobViewModel);
         }
+
 
         public IActionResult Detail(int id)
         {
--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,5 +1,7 @@
 --Part 1
-
+--Column ID:int
+--Column Name:longtext
+--Column EmployerID: int
 --Part 2
 
 --Part 3
