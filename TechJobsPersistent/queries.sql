791180957309063e12273320fc8eb03ef6955e6a
Kenneth Jackson <kcjackson1985@gmail.com>
2021-12-13 22:31:03 -0500

    Query

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,12 +1,12 @@
 --Part 1
---Column ID:int
---Column Name:longtext
---Column EmployerID: int
+Column Id: int
+Column Name: longtext
+Column EmployerID: int
 --Part 2
---SELECT Name FROM Employers
---WHERE location='St. Louis City';
+SELECT Name FROM Employers
+WHERE location='St. Louis City';
 --Part 3
---SELECT Name, Description FROM skills
---LEFT JOIN jobskills on skills.id = jobskills.skillsid
---WHERE jobskills.jobid IS NOT NULL
---ORDER BY Name ASC, Description ASC;
+SELECT Name, Description FROM skills
+LEFT JOIN jobskills on skills.id = jobskills.skillsid
+WHERE jobskills.jobid IS NOT NULL
+ORDER BY Name ASC, Description ASC;
