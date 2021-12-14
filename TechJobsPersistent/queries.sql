f3a5dd442f8b10a7f93a49d412d1d11177a6641d
Kenneth Jackson <kcjackson1985@gmail.com>
2021-12-13 22:32:39 -0500

    Double quotes

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -2,9 +2,11 @@
 Column Id: int
 Column Name: longtext
 Column EmployerID: int
+
 --Part 2
 SELECT Name FROM Employers
-WHERE location='St. Louis City';
+WHERE location="St. Louis City";
+
 --Part 3
 SELECT Name, Description FROM skills
 LEFT JOIN jobskills on skills.id = jobskills.skillsid
