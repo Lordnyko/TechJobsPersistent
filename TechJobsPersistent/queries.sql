40d0ee6f9637930af4232d09cd5e0f6649ece72f
Kenneth Jackson <kcjackson1985@gmail.com>
2021-12-13 22:25:39 -0500

    Forgot to add Queries!

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -3,6 +3,10 @@
 --Column Name:longtext
 --Column EmployerID: int
 --Part 2
-
+--SELECT Name FROM Employers
+--WHERE location='St. Louis City';
 --Part 3
-
+--SELECT Name, Description FROM skills
+--LEFT JOIN jobskills on skills.id = jobskills.skillsid
+--WHERE jobskills.jobid IS NOT NULL
+--ORDER BY Name ASC, Description ASC;
