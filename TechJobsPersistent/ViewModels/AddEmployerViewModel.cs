c49921d685d2ba8e6958fd3e52e389a75187f3a0
Kenneth Jackson <kcjackson1985@gmail.com>
2021-12-14 21:08:07 -0500

    Validation wasn't woring

--- TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using TechJobsPersistent.Models;
 
 namespace TechJobsPersistent.ViewModels
 {
     public class AddEmployerViewModel
     {
-
         [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Location is required.")]
         public string Location { get; set; }
 
-        public AddEmployerViewModel(string name, string location)
-        {
-            Name = name;
-            Location = location;
-        }
 
         public AddEmployerViewModel() { }
+
+        public AddEmployerViewModel(Employer employer)
+        {
+            Name = employer.Name;
+            Location = employer.Location;
+        }
     }
 }
--- TechJobsPersistent/ViewModels/AddJobViewModel.cs
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -10,29 +10,28 @@ namespace TechJobsPersistent.ViewModels
 {
     public class AddJobViewModel
     {
-        [Required(ErrorMessage = "Job name is required")]
         public string Name { get; set; }
-
-        [Required(ErrorMessage = "Employer is required. Add new employer if necessary.")]
         public int EmployerId { get; set; }
         public List<SelectListItem> Employers { get; set; }
-
         public List<Skill> Skills { get; set; }
 
-        public AddJobViewModel(List<Employer> employers, List<Skill> jobSkills)
+
+
+        public AddJobViewModel(List<Employer> employers, List<Skill> skills)
         {
             Employers = new List<SelectListItem>();
 
-            foreach (var employer in employers)
+            foreach (Employer employer in employers)
             {
-                Employers.Add(new SelectListItem
-                {
-                    Value = employer.Id.ToString(),
-                    Text = employer.Name
-                });
+                Employers.Add(
+                    new SelectListItem
+                    {
+                        Value = employer.Id.ToString(),
+                        Text = employer.Name
+                    }
+                );
             }
-
-            Skills = jobSkills;
+            Skills = skills;
         }
 
         public AddJobViewModel() { }
